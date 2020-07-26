ee2ba593fb39ccfec4c7bea10e0fbad16b018834
Umair <umairnehri9747@Gmail.com>
2020-07-26 19:49:48 +0530

    Updated zsh.sh
    Updated the file with some minor changes in design and logging of installation output.

--- zsh.sh
+++ zsh.sh
@@ -1,24 +1,49 @@
 #!/bin/bash
 # Installs Zsh, Oh-My-Zsh, wget and git
 
-echo Updating package list
-sudo apt update >/dev/null
+# Description 
+echo -e "\e[1;31m ZSH Installer by StrawHatHacker \e[0m"
+printf "\n"
+echo "The installation is starting , please wait for atleast 2 to 3 minutes"
 
-echo Installing ZSH
+sleep 5
+
+touch log.txt
+
+# Updates the package list
+echo Updating package list...
+echo "Update for package list" > log.txt
+printf "\n" && printf "\n" >> log.txt
+sudo apt update >> log.txt
+
+# Installs ZSH
+echo Installing ZSH...
+printf "\n" >> log.txt
+echo "Installation of ZSH" >> log.txt
+printf "\n" && printf "\n" >> log.txt
 sudo apt install zsh -y 2>/dev/null
 
-echo Installing wget and git
+# Installs wget and git
+echo Installing wget and git...
+printf "\n" >> log.txt
+echo "Installation of wget and git" >> log.txt
+printf "\n" && printf "\n" >> log.txt
 sudo apt install wget -y 2>/dev/null
 sudo apt install git -y 2>/dev/null
 
-echo Installing OhMyZsh
+# Installs OhMyZsh
+echo Installing OhMyZsh...
+printf "\n" >> log.txt
+echo "Installation of OhMyZsh" >> log.txt
+printf "\n" && printf "\n" >> log.txt
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh 2>/dev/null
 cp ~/.zshrc ~/.zshrc.orig
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
-echo Making ZSH the default shell
+clear 
+
+echo -e "\e[1;31m ZSH has been successfully installed! \e[0m"
 chsh -s $(which zsh)
 
-echo Done!
 zsh
 exit 0

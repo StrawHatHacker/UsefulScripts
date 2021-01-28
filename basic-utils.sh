dbef213caace42ce0fab120f56ea4a259789b4fa
strawhathacker <ntahsp@gmail.com>
2021-01-28 13:38:13 +0200

    Python required packages

--- basic-utils.sh
+++ basic-utils.sh
@@ -31,6 +31,9 @@ printf "\n" && printf "\n" >>log.txt
 echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
 wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
 sudo apt update >>log.txt
+# Installing required python packages
+sudo pip3 install -U setuptools 2>/dev/null
+sudo pip3 install psutil 2>/dev/null
 sudo apt install bpytop -y 2>/dev/null
 
 # Installs node.js LTS
